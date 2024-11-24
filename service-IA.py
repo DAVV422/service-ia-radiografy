a2f0223daf1a3248be053e00060fe78191ef8f90
DAVV422 <diegoalberto42216@gmail.com>
2024-11-24 06:22:55 -0400

    refactor host and port

--- service-IA.py
+++ service-IA.py
@@ -301,4 +301,5 @@ def clasificar_imagen():
 #-----------------------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+    #app.run(debug=True)
