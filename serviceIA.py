5fd797b92128ea439287b43e5998d6d441d9b44f
DAVV422 <diegoalberto42216@gmail.com>
2024-11-27 21:00:07 -0400

    refactor guardar imagen

--- serviceIA.py
+++ serviceIA.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify, send_file, send_from_directory
 from torchvision import transforms
 from PIL import Image, ImageDraw
-from joblib import load
+from datetime import datetime
 from flask_cors import CORS
+from joblib import load
 import numpy as np
 import base64
 import torch
@@ -29,7 +30,6 @@ index2name = {
     5: "Nodule",
     6: "Pneumonia",
     7: "Pneumothorax",
-    # Añadir más etiquetas según tu modelo
 }
 
 # Preprocesamiento de la imagen
@@ -105,7 +105,25 @@ def detectar():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
+# -------------- Función para obtener el diagnostico médico de la IA -----------------------
+def obtener_diagnostico(nombre_diagnostico):
+    # Diccionario de diagnósticos y descripciones
+    diagnosticos = {
+        "Atelectasis": "Atelactasis: Colapso del tejido pulmonar con pérdida de volumen. Surge cuando los pequeños sacos de aire dentro del pulmón, los alvéolos, pierden aire.",
+        "Cardiomegaly": "Cardiomegalia: Aumento del tamaño cardiaco por hipertrofia o dilatación. Suele ser un signo de enfermedad cardiaca o síntoma de otra afección.",
+        "Effusion": "Efusion Pleural: Acumulación anormal del líquido entre las capas delgadas del tejido (pleura) que recubre el pulmón y la pared de la cavidad torácica.",
+        "Infiltrate": "Infiltrado Pulmonar: Sombras pulmonares anormales, están son el reflejo de una enfermedad o secuela de una enfermedad pulmonar.",
+        "Mass": "Tumores Mediastianles: Masas o neoplasias que se forman en el mediastino, una zona en la mitad del tórax que separa los pulmones.",
+        "Nodule": "Nodulos Pulmonares: Densidad focal relativamente pequeña en el pulmón. Un nódulo pulmonar solitario (NPS) o lesión en forma de moneda, es una masa en el pulmón de menos de tres centímetros de diámetro.",
+        "Pneumonia": "Neumonía: Infección en uno o ambos pulmones. Causa que los alvéolos pulmonares se llenen de líquido o pus.",
+        "Pneumothorax": "Neumotórax: Presencia de aire en el espacio pleural que causa colapso pulmonar parcial o completo."
+    }
+     
+    # Devolver el diagnóstico correspondiente o un mensaje de error si no se encuentra
+    return diagnosticos.get(nombre_diagnostico, "Diagnóstico no encontrado.")    
+    
+    
+    
 #---------------- Devuelve un JSON con la imagen y la prediccion -------------------
 @app.route('/detectar2', methods=['POST'])
 def detectar2():
@@ -113,6 +131,17 @@ def detectar2():
         return jsonify({"error": "No se encontró ninguna imagen"}), 400
     
     archivo_imagen = request.files['imagen']
+    
+    if archivo_imagen.filename == '':
+        return "No se seleccionó ningún archivo", 400
+    
+    # Obtener el nombre original del archivo
+    nombre_imagen = archivo_imagen.filename
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    nombre_imagen = f"{timestamp}_{nombre_imagen}"
+    nombre_imagen = nombre_imagen.replace(" ", "_") 
+    diagnostico = ""
+    
     try:
         # Leer y preprocesar la imagen
         imagen = Image.open(io.BytesIO(archivo_imagen.read())).convert('RGB')
@@ -141,6 +170,8 @@ def detectar2():
                 label = 0  # Etiqueta por defecto si el índice es inválido
             name = index2name[label]
             names.append(name)
+            # Obtener el diagnostico de la imagen
+            diagnostico = obtener_diagnostico(name)
         
         # Dibujar las cajas sobre la imagen original
         draw = ImageDraw.Draw(imagen)
@@ -148,6 +179,11 @@ def detectar2():
             x1, y1, x2, y2 = map(int, box)
             draw.rectangle([x1, y1, x2, y2], outline="red", width=3)  # Caja en rojo
             draw.text((x1, y1 - 10), name, fill="red")  # Nombre encima de la caja
+            
+        # Guardar la imagen en la carpeta
+        ruta_imagen = os.path.join(UPLOAD_FOLDER, nombre_imagen)
+        ruta_imagen = ruta_imagen.replace("\\", "/") 
+        imagen.save(ruta_imagen)
         
         # Guardar la imagen en un buffer
         buffer = io.BytesIO()
@@ -160,6 +196,8 @@ def detectar2():
         # Preparar respuesta JSON
         respuesta = {
             "prediccion": [{"nombre": names[i], "puntaje": float(scores[top_indices[i]])} for i in range(len(names))],
+            "diagnostico": diagnostico,
+            "ruta_imagen": ruta_imagen,
             "imagen": imagen_base64
         }
         
@@ -270,5 +308,5 @@ def detectarjson():
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=5000)
     #app.run(debug=True)
